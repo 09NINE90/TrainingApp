fd1699f9dc70bd6d847c9008fb943105ff789ab9
09NINE90 <razumov.aleksei101@yandex.ru>
2024-02-13 18:57:29 +0500

    going to the user's page

--- src/main/java/com/example/security20/controller/UserController.java
+++ src/main/java/com/example/security20/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.security20.controller;
 
 import com.example.security20.dto.UserDTO;
+import com.example.security20.entity.User;
 import com.example.security20.entity.UserPhysicalParameters;
 import com.example.security20.repository.UserPhysicalParametersRepository;
 import com.example.security20.repository.UserRepository;
@@ -11,31 +12,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
 
-//@Controller
-//@RequiredArgsConstructor
-//@RequestMapping("/user")
-//public class UserController {
-//    private final UserService userService;
-//    @Autowired
-//    private UserPhysicalParametersRepository userPhysicalParametersRepository;
-//    @GetMapping("/create")
-//    public String userCreate(Model model){
-//        model.addAttribute("user",new UserDTO());
-//        return "signup";
-//    }
-//    @PostMapping("/create")
-//    public String userSave(@ModelAttribute("user") UserDTO userDTO){
-//        userService.createUser(userDTO);
-//        return "redirect:/login";
-//    }
-//
-//}
-//
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+    @Autowired
+    private UserPhysicalParametersRepository userPhysicalParametersRepository;
+    @GetMapping("/{userId}")
+    public String userCreate(@PathVariable("userId") Long id, Model model){
+        Optional<User> userOptional = userService.getUserById(id);
+        User user = userOptional.get();
+        List<UserPhysicalParameters> userPhysicalParametersList = userPhysicalParametersRepository.findPhysicalParametersByUserId(user.getId());
+        model.addAttribute("user", user);
+        model.addAttribute("userPhysicalParametersList", userPhysicalParametersList);
+        return "userPage";
+    }
+
+}
+
--- src/main/java/com/example/security20/service/UserService.java
+++ src/main/java/com/example/security20/service/UserService.java
@@ -1,14 +1,17 @@
 package com.example.security20.service;
 
 import com.example.security20.dto.UserDTO;
+import com.example.security20.entity.User;
 import com.example.security20.entity.UserPhysicalParameters;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
-    UserDTO createUser(UserDTO userDTO);
+    public UserDTO createUser(User user);
 
-    List<UserDTO> findAllUsers();
+    public List<User> findAllUsers() ;
     void saveUserPhysicalParameters(UserPhysicalParameters userPhysicalParameters);
+    public Optional<User> getUserById(Long id);
 
 }
