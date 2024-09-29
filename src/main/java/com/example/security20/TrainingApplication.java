40a3ac478fdd6744420f520f19a050b9dc199d30
09NINE90 <razumov.aleksei101@yandex.ru>
2024-09-29 15:53:33 +0500

    refactor

--- src/main/java/com/example/security20/TrainingApplication.java
+++ src/main/java/com/example/security20/TrainingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Security20Application {
+public class TrainingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Security20Application.class, args);
+        SpringApplication.run(TrainingApplication.class, args);
     }
 
 }
